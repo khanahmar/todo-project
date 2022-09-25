6d3ae53830dc806a1f15b49b8f445eec411a4aa3
Ahmar khan <ahmarbilal223344@gmail.com>
2022-09-25 02:16:27 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const formE1 = document.querySelector(".form");
+const inputE1 = document.querySelector(".input");
+const ulE1 = document.querySelector(".list");
+
+formE1.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  todoList();
+});
+
+function todoList() {
+  let newTask = inputE1.value;
+  const liE1 = document.createElement("li");
+  liE1.innerText = newTask;
+  ulE1.appendChild(liE1);
+  inputE1.value = "";
+  const checkBtnE1 = document.createElement("div");
+  checkBtnE1.innerHTML = ` <i class="fa-solid fa-square-check">`;
+  liE1.appendChild(checkBtnE1);
+  const trashBtnE1 = document.createElement("div");
+  trashBtnE1.innerHTML = ` <i class="bi bi-trash-fill">`;
+  liE1.appendChild(trashBtnE1);
+
+  checkBtnE1.addEventListener("click", () => {
+    liE1.classList.toggle("checked");
+  });
+
+  trashBtnE1.addEventListener("click", (e) => {
+    liE1.remove();
+  });
+}
