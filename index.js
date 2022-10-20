b2c9194c6458a6d8026d7ec625e6f268d86af4d4
Ahmar khan <ahmarbilal223344@gmail.com>
2022-10-20 22:41:33 +0500

    changes made

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const formE1 = document.querySelector(".form");
 const inputE1 = document.querySelector(".input");
-const ulE1 = document.querySelector(".list");
+let ulE1 = document.querySelector(".list");
 
 formE1.addEventListener("submit", (event) => {
   event.preventDefault();
-
   todoList();
 });
 
 function todoList() {
   let newTask = inputE1.value;
+
   const liE1 = document.createElement("li");
   liE1.innerText = newTask;
   ulE1.appendChild(liE1);
@@ -20,7 +20,6 @@ function todoList() {
   const trashBtnE1 = document.createElement("div");
   trashBtnE1.innerHTML = ` <i class="bi bi-trash-fill">`;
   liE1.appendChild(trashBtnE1);
-
   checkBtnE1.addEventListener("click", () => {
     liE1.classList.toggle("checked");
   });
@@ -28,4 +27,12 @@ function todoList() {
   trashBtnE1.addEventListener("click", (e) => {
     liE1.remove();
   });
+}
+
+function myStorage() {
+  localStorage.setItem("items", ulE1);
+}
+
+function getStorage() {
+  localStorage.getItem("item");
 }
